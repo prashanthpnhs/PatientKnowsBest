c9a9091f492a7aa48af93d05ec9bb046c23fea44
suvithaSivaramanNHS <suvitha.sivaraman@nhs.net>
2020-04-23 15:28:26 +0100

    Remove duplicated diabetes project

--- fihr-exporter/src/main/java/org/endeavourhealth/fihrexporter/exception/FihrExporterException.java
+++
@@ -1,7 +0,0 @@
-package org.endeavourhealth.fihrexporter.exception;
-
-public class FihrExporterException extends RuntimeException {
-    public FihrExporterException(String message) {
-        super(message);
-    }
-}
